ba39f08b074ec85b51ba3d06065af2e8b7dbfd22
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-10 09:20:10 +0530

    Update notes_sql.py

--- innexiaBot/Database/notes_sql.py
+++ innexiaBot/Database/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from Innexia.modules.helper_funcs.msg_types import Types
-from Innexia.Database import BASE, SESSION
+from innexiaBot.Handlers.msg_types import Types
+from innexiaBot.Database import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 
