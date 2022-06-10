b3b11c207736d61170b95c3ddc654b17693bc95b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-10 09:16:44 +0530

    Update welcome_sql.py

--- innexiaBot/Database/welcome_sql.py
+++ innexiaBot/Database/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from Innexia.modules.helper_funcs.msg_types import Types
-from Innexia.Database import BASE, SESSION
+from innexiaBot.Handlers.msg_types import Types
+from innexiaBot.Database import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
