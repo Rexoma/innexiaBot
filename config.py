b325ced4f2c3f75a353806de6f8006e03bfb7009
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-10 16:32:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 from os import environ
 
-DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
 API_ID = int(os.environ.get("API_ID", None))
 API_HASH = os.environ.get("API_HASH", None)
 DB_URI = os.environ.get("DATABASE_URL")
@@ -22,6 +21,8 @@ NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
 DEL_CMDS = bool(os.environ.get("DEL_CMDS", True))
 INFOPIC = bool(os.environ.get("INFOPIC", True))
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
+DEV_USERS = {int(x) for x in os.environ.get("DEV_USERS", "").split()}
+SUDOERS = {int(x) for x in os.environ.get("SUDOERS", "").split()}
 TOKEN = os.environ.get("TOKEN", None)
 DONATION_LINK = os.environ.get("DONATION_LINK", None)
 
